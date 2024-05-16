6aea4abedd33fb22446c136e6c28873d2e6df9ff
TvMroc <1207837@student.roc-nijmegen.nl>
2024-05-16 11:04:34 +0200

    counter

--- html/index.php
+++ html/index.php
@@ -4,6 +4,7 @@
     </head>
     <body>
         <p id="yes">world</p>
+        <p id="counter">0</P>
         <button id="rmove" onclick="RMove()" ></button>
         <script src="js/main.jsx"></script>
     </body>
--- html/js/main.jsx
+++ html/js/main.jsx
@@ -1,8 +1,10 @@
 console.log("hello world");
 x = document.getElementById('yes');
 x.textContent = "hello";
-
+var count = 0;
 const RMove = () => {
     document.querySelector("#rmove").style.marginLeft = Math.floor(Math.random() * 1600);
     document.querySelector("#rmove").style.marginTop = Math.floor(Math.random() * 800);
+    count++;
+    document.querySelector("#counter").textContent = count;
 }
