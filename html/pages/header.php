885edf223012bcc6950d9fd640da0e342d8fc302
TvMroc <1207837@student.roc-nijmegen.nl>
2024-05-17 14:31:04 +0200

    header

--- html/pages/header.php
+++ html/pages/header.php
@@ -1,8 +1,21 @@
 <header>
-    <div class="logo">
-        <img src="../images/logo.png">
+    <div>
+        <div class="bar">
+            <img src="../images/logo.png">
+            <div>
+                <img src="../images/Rectangle24.png">
+                <img src="../images/Ellipse3.png">
+            </div>
+        </div>
     </div>
-    <div class="nav">
-        <a href="dashboard.php">Home</a>
+    <div>
+        <div class="bar">
+            <a href="">Zomervakantie</a>
+            <a href="">Last Minute</a>
+            <a href="">Verre Reizen</a>
+            <a href="">Vliegtickets</a>
+            <a href="">Stedetrips</a>
+            <a href="">Overig</a>
+        </div>
     </div>
 </header>
