47a896743bf6fada99179bc68923ec5d43c42d28
TvMroc <1207837@student.roc-nijmegen.nl>
2024-05-17 12:47:36 +0200

    win

--- html/index.php
+++ html/index.php
@@ -2,10 +2,15 @@
     <head>
         <link rel="stylesheet" href="css/main.css">
     </head>
-    <body>
-        <p id="yes">world</p>
-        <p id="counter">0</P>
-        <button id="rmove" onclick="RMove()" ></button>
+    <body onclick="count()">
+        <div class="info">
+            <button onclick="timer()">cps test 10s</button>
+            <p id="yes">world</p>
+            <p id="counter">0</P>
+            <p id="cps">0</P>
+        </div>
+        <button id="rmove" onclick="RMove()" >random</button>
+        <div class="square"></div>
         <script src="js/main.jsx"></script>
     </body>
 </html>
--- html/js/main.jsx
+++ html/js/main.jsx
@@ -1,10 +1,39 @@
 console.log("hello world");
 x = document.getElementById('yes');
 x.textContent = "hello";
-var count = 0;
+var randomcount = 0;
+var cps = 0;
+var clickcount = 0;
+var time = false;
 const RMove = () => {
-    document.querySelector("#rmove").style.marginLeft = Math.floor(Math.random() * 1600);
-    document.querySelector("#rmove").style.marginTop = Math.floor(Math.random() * 800);
-    count++;
-    document.querySelector("#counter").textContent = count;
+    document.querySelector("#rmove").style.marginLeft = Math.floor(Math.random() * 100)+"dvw";
+    document.querySelector("#rmove").style.marginTop = Math.floor(Math.random() * 100)+"dvh";
+    randomcount++;
+    document.querySelector("#counter").textContent = randomcount;
+    
+    setTimeout(function () {
+        const element1 = document.querySelector("#rmove"); 
+        const element2 = document.querySelector(".square"); 
+        
+        const rect1 = element1.getBoundingClientRect(); 
+        const rect2 = element2.getBoundingClientRect(); 
+        
+        if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) { 
+            alert("You won");
+        }
+    }, 210);
+}
+const timer = () => {
+    time = true;
+    setTimeout(function () {
+        time = false;
+        cps = clickcount/10;
+        document.querySelector("#cps").textContent = cps+" "+clickcount;
+        clickcount = 0;
+    }, 10000);
+}
+const count = () => {
+    if (time) {
+        clickcount++;
+    }
 }
