d8742e17c7b4fc68744f02be833fcf5718ce4c73
TvMroc <1207837@student.roc-nijmegen.nl>
2024-05-30 17:22:59 +0200

    usestate

--- html/pages/header.php
+++ html/pages/header.php
@@ -1,17 +1,17 @@
 <header>
     <div>
         <div class="bar">
-            <a href='../index.php'><img src="../images/logo.png"></a>
+            <a href='../'><img src="../images/logo.png"></a>
             <div>
                 <?php
-                echo '<a href="./' . $_SESSION['pages'] . 'favorites.php"><img src="../images/Star 2.jpg"></a>';
+                    echo '<a href="./' . $_SESSION['pages'] . 'favorites.php"><img src="../images/Star 2.jpg"></a>';
                 ?>
                 <img src="../images/Rectangle24.png">
                 <?php
                     if (isset($_SESSION['user'])) {
                         echo '<img class="account" src="../images/bob.png">';
                     } else {
-                        echo '<a href="'. $_SESSION['pages'] . 'login.php"><img class="account" src="../images/midway.png"></a>';
+                        echo '<a href="'. $_SESSION['pages'] . 'login.php"><img class="account" src="../images/account.png"></a>';
                     }
                 ?>
             </div>
@@ -20,12 +20,12 @@
     <div>
         <div class="bar">
             <?php
-            echo '<a href="./' . $_SESSION['pages'] . 'zomervakantie.php">Zomervakantie</a>';
-            echo '<a href="./' . $_SESSION['pages'] . 'lastminute.php">Last Minute</a>';
-            echo '<a href="./' . $_SESSION['pages'] . 'verrereizen.php">Verre Reizen</a>';
-            echo '<a href="./' . $_SESSION['pages'] . 'vliegtickets.php">Vliegtickets</a>';
-            echo '<a href="./' . $_SESSION['pages'] . 'stedetrips.php">Stedetrips</a>';
-            echo '<a href="./' . $_SESSION['pages'] . 'overig.php">Overig</a>';
+                echo '<a href="./' . $_SESSION['pages'] . 'zomervakantie.php">Zomervakantie</a>';
+                echo '<a href="./' . $_SESSION['pages'] . 'lastminute.php">Last Minute</a>';
+                echo '<a href="./' . $_SESSION['pages'] . 'verrereizen.php">Verre Reizen</a>';
+                echo '<a href="./' . $_SESSION['pages'] . 'vliegtickets.php">Vliegtickets</a>';
+                echo '<a href="./' . $_SESSION['pages'] . 'stedetrips.php">Stedetrips</a>';
+                echo '<a href="./' . $_SESSION['pages'] . 'overig.php">Overig</a>';
             ?>
         </div>
     </div>
