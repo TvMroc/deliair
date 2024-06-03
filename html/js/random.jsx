80902e419fa18b116b7862a253544972b2f17294
Bert <98714764+BertNK@users.noreply.github.com>
2024-06-03 14:40:17 +0200

    update

--- html/js/random.jsx
+++ html/js/random.jsx
@@ -3,19 +3,19 @@ x = document.getElementById('yes');
 x.textContent = "hello";
 var randomcount = 0;
 const RMove = () => {
-    document.querySelector("#rmove").style.marginLeft = Math.floor(Math.random() * 90)+"dvw";
-    document.querySelector("#rmove").style.marginTop = Math.floor(Math.random() * 90)+"dvh";
+    document.querySelector("#rmove").style.marginLeft = Math.floor(Math.random() * 90) + "dvw";
+    document.querySelector("#rmove").style.marginTop = Math.floor(Math.random() * 90) + "dvh";
     randomcount++;
     document.querySelector("#counter").textContent = randomcount;
-    
+
     setTimeout(function () {
-        const element1 = document.querySelector("#rmove"); 
-        const element2 = document.querySelector(".square"); 
-        
-        const rect1 = element1.getBoundingClientRect(); 
-        const rect2 = element2.getBoundingClientRect(); 
-        
-        if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) { 
+        const element1 = document.querySelector("#rmove");
+        const element2 = document.querySelector(".square");
+
+        const rect1 = element1.getBoundingClientRect();
+        const rect2 = element2.getBoundingClientRect();
+
+        if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x && rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
             alert("You won");
         }
     }, 210);
