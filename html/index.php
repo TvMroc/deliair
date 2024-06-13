1f72b9bd3511e77347ff85d5efe423b155395bd3
Bert <98714764+BertNK@users.noreply.github.com>
2024-06-13 11:01:13 +0200

    egehtjdtbldgb
    egnbgd

--- html/index.php
+++ html/index.php
@@ -23,20 +23,25 @@ $_SESSION['pages'] = "pages/";
     <div class="indexdoos1" id='homepage-header'>
         <div class="indexblok1">
             <div class="indexbalk1">
-                <div onclick="selection('hideindexbalkbutton', 'indexbalkbutton1')" class="indexbalkbutton"><a class="indexbuttontekst1">Countries</a></div>
+                <div onclick="selection('hideindexbalkbutton', 'indexbalkbutton1')" class="indexbalkbutton"><a class="indexbuttontekst1">Landen</a></div>
                 <div onclick="selection('hideindexbalkbutton', 'indexbalkbutton2')" class="indexbalkbutton"><a class="indexbuttontekst1">-</a></div>
                 <div onclick="selection('hideindexbalkbutton', 'indexbalkbutton3')" class="indexbalkbutton"><a class="indexbuttontekst1">-</a></div>
                 <div onclick="selection('hideindexbalkbutton', 'indexbalkbutton4')" class="indexbalkbutton"><a class="indexbuttontekst1">-</a></div>
                 <div onclick="selection('hideindexbalkbutton', 'indexbalkbutton5')" class="indexbalkbutton"><a class="indexbuttontekst1">Info</a></div>
             </div>
             <div class="indexblok2">
-                <div class="indexbalkbutton1 hideindexbalkbutton">13456789</div>
-                <div class="indexbalkbutton2 hideindexbalkbutton">234567</div>
-                <div class="indexbalkbutton3 hideindexbalkbutton">33456</div>
-                <div class="indexbalkbutton4 hideindexbalkbutton">423456</div>
-                <div class="indexbalkbutton5 hideindexbalkbutton">51234</div>
+                <div class="indexbalkbutton1 hideindexbalkbutton"><div class="indexbloktekst"><a class="indextekst2">-</a></div></div>
+                <div class="indexbalkbutton2 hideindexbalkbutton"><div class="indexbloktekst"><a class="indextekst2">-</a></div></div>
+                <div class="indexbalkbutton3 hideindexbalkbutton"><div class="indexbloktekst"><a class="indextekst2">-</a></div></div>
+                <div class="indexbalkbutton4 hideindexbalkbutton"><div class="indexbloktekst"><a class="indextekst2">-</a></div></div>
+                <div class="indexbalkbutton5 hideindexbalkbutton"><div class="indexbloktekst"><a class="indextekst2">3 dingen die je moet weten over ons boekingbureau.</a>
+                <a class="indextekst2">Minderjarige moeten minstens een ouder/voogd meenemen op vluchten.</a>
+                <a class="indextekst2">Een all-inclusive moet tenminste 3 weken van tevoren geannuleert worden.</a>
+                <a class="indextekst2">Je moet minstens 30 minuten van te voren inchecken bij de vliegveld</a>
             </div>
         </div>
     </div>
+</div>
+</div>
 </body>
 </html>
