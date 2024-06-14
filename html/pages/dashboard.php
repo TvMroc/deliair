4186b9733dd43d0fe8edf34599af68b3775fe41f
Bert <98714764+BertNK@users.noreply.github.com>
2024-06-14 10:57:55 +0200

    ohufrof
    bg

---
+++ html/pages/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+$_SESSION['pages'] = "";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>DeliAir</title>
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+</body>
+
+</html>
