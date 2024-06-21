c124f9fecb1d6a37ec23e3940d124ddf1b10946e
Bert <98714764+BertNK@users.noreply.github.com>
2024-06-21 12:56:35 +0200

    oguv

--- html/pages/stedetrips.php
+++ html/pages/stedetrips.php
@@ -27,40 +27,43 @@ $_SESSION['pages'] = "";
             <a class="boekknop" href="booking.php">Boeken</a>
         </div>
         <div class="stedetripsblokin2">
-            <img src="../images/denmark.jpg" class="countryimg">
+            <img src="../images/amsterdam.jpg" class="countryimg">
         </div>
     </div>
     <div class="stedetripsblok1">
         <div class="landenblokin1">
-            <a class="landentekst1">Over Denenmarken:</a>
+            <a class="landentekst1">Over Helsinki:</a>
             <a class="landentekst2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin
                 volutpat libero, eget ultricies augue dignissim id. In vitae pretium risus. Phasellus pulvinar lacus
                 sodales libero rhoncus, imperdiet mollis nulla pellentesque.</a>
+                <a class="boekknop" href="booking.php">Boeken</a>
         </div>
         <div class="landenblokin2">
-            <img src="../images/denmark.jpg" class="countryimg">
+            <img src="../images/helsinki.jpg" class="countryimg">
         </div>
     </div>
     <div class="stedetripsblok1">
         <div class="landenblokin1">
-            <a class="landentekst1">Over Denenmarken:</a>
+            <a class="landentekst1">Over Stockholm:</a>
             <a class="landentekst2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin
                 volutpat libero, eget ultricies augue dignissim id. In vitae pretium risus. Phasellus pulvinar lacus
                 sodales libero rhoncus, imperdiet mollis nulla pellentesque.</a>
+                <a class="boekknop" href="booking.php">Boeken</a>
         </div>
         <div class="landenblokin2">
-            <img src="../images/denmark.jpg" class="countryimg">
+            <img src="../images/stockholm.jpg" class="countryimg">
         </div>
     </div>
     <div class="stedetripsblok1">
         <div class="landenblokin1">
-            <a class="landentekst1">Over Denenmarken:</a>
+            <a class="landentekst1">Over Hamburg:</a>
             <a class="landentekst2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin
                 volutpat libero, eget ultricies augue dignissim id. In vitae pretium risus. Phasellus pulvinar lacus
                 sodales libero rhoncus, imperdiet mollis nulla pellentesque.</a>
+                <a class="boekknop" href="booking.php">Boeken</a>
         </div>
         <div class="landenblokin2">
-            <img src="../images/denmark.jpg" class="countryimg">
+            <img src="../images/hamburg.jpg" class="countryimg">
         </div>
     </div>
 </div>
