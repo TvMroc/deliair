8475960ee347dff4cd78b2b166fa43e170f9fa8b
TvMroc <1207837@student.roc-nijmegen.nl>
2024-06-21 13:08:36 +0200

    favourites

--- html/pages/verrereizen.php
+++ html/pages/verrereizen.php
@@ -10,6 +10,7 @@ $_SESSION['pages'] = "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/main.css">
     <title>DeliAir</title>
+    <script src="../js/selector.jsx"></script>
 </head>
 
 <body id="bodyindex">
@@ -22,8 +23,30 @@ $_SESSION['pages'] = "";
                 <a class="tekstvoorverreizen">Dit zijn de landen waar wij je naar uit kunnen vliegen.</a>
             </div>
             <div class="verrereizenblokje2">
-                <a class="landenlol">Amerika, Duitsland, Belgie, Finland, Sweden, Denenmarken, Indonesie, Afrika, Spanje, Italie, Groot-Brittanie, Ierland, Schotland, Wales, Ijsland, Dubai, Turkije, UAE, Saudi Arabie, Qatar, Singapore, Kenia, Japan. </a>
+                <a class="landenlol">Finland, Sweden, Denenmarken, Indonesie, Afrika, Spanje, Italie, Groot-Brittanie, Ierland, Schotland, Wales, Ijsland, Dubai, Turkije, UAE, Saudi Arabie, Qatar, Singapore, Kenia, Japan. </a>
+                <?php
+                    include 'conn.php';
+
+                    $sql = 'SELECT name, id, description FROM locations';
+                    $stmt = $connection->query($sql);
+
+                    while ($row = $stmt->fetch()) {
+                        echo '<div id="' . $row['name'] . '" onclick="selection(\'land\', \'land' . $row['id'] . '\')">';
+                        echo '<p>' . $row['name'] . '</p>';
+                        echo '</div>';
+                    }
+                ?>
             </div>
+            <?php
+                    include 'conn.php';
+
+                    $sql = 'SELECT name, id, description FROM locations';
+                    $stmt = $connection->query($sql);
+
+                    while ($row = $stmt->fetch()) {
+                        echo '<p class="land" id="land' . $row['id'] . '">' . $row['description'] . '</p>';
+                    }
+                ?>
         </div>
     </div>
 </body>
