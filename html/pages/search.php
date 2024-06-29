2727b2981f9496b9f4f5a56788783ec90812e012
Bert <98714764+BertNK@users.noreply.github.com>
2024-06-29 13:11:07 +0200

    search gefixtr
    adsw

--- html/pages/search.php
+++ html/pages/search.php
@@ -10,30 +10,31 @@ $_SESSION['pages'] = "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/main.css">
     <title>DeliAir Search</title>
+    <script src="../js/search.js"></script>
 </head>
 
 <body>
     <?php
     include 'header.php';
     ?>
+    <a class="back" href="../index.php">← Home<a>
     <div class="searchblok">
-        <div class>
-        <h1 class="searchbloktekstheader">Geboekte vluchten</h1>
-        <input class="search" type="text" placeholder="search" id="search" onkeyup="bookSearch('booking-item')"><br>
+        <h1 class="searchbloktekstheader">Flight locations</h1>
+        <input class="searchbar" type="text" placeholder="Search the location of your desire" id="search" onkeyup="bookSearch('locations-item')"><br>
         <?php
             include 'conn.php';
 
-            $sql = 'SELECT booking_id, id, country, start_date, end_date FROM bookings';
+            $sql = 'SELECT id, name, description FROM locations';
             $stmt = $connection->query($sql);
 
             while ($row = $stmt->fetch()) {
-                echo '<div id="' . $row['country'] . '" class="booking-item">';
-                echo '<p">' . $row['country'] . '</p>';
-                echo '<p>' . $row['start_date'] . '</p><p>' . $row['end_date'] . '</p>';
+                echo '<div id="bookingcountry' . $row['name'] . '" class="locations-item">';
+                echo '<p">' . $row['name'] . '</p>';
+                echo '<p">' . $row['id'] . '</p>';
+                echo '<a href="booking.php?id='  . '">Book the trip</a>';
                 echo '</div>';
             }
         ?>
     </div>
-</div>
 </body>
 </html>
